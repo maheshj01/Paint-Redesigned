70e54bada8e6704457f4743ea9bdd174db0f90c4
Mahesh Jamdade <maheshmn121@gmail.com>
2020-05-24 23:49:03 +0530

    added brush size modifier and minor changes

--- lib/whiteboard.dart
+++ lib/whiteboard.dart
@@ -13,13 +13,28 @@ class WhiteBoard extends StatefulWidget {
   _WhiteBoardState createState() => _WhiteBoardState();
 }
 
-class _WhiteBoardState extends State<WhiteBoard> {
+class _WhiteBoardState extends State<WhiteBoard>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this)
+          ..addListener(() => setState(() {}));
+    animation = Tween(begin: -200.0, end: 0.0).animate(_controller);
+  }
+
   void changeColor(Color color) {
     setState(() => pickerColor = color);
   }
 
   Color pickerColor = Colors.black;
   BlendMode blendMode = BlendMode.softLight;
+  double _sliderValue = 10.0;
+  AnimationController _controller;
+  Animation<double> animation;
+  bool isShown = false;
   @override
   Widget build(BuildContext context) {
     List<Offset> localList = [];
@@ -61,80 +76,164 @@ class _WhiteBoardState extends State<WhiteBoard> {
           });
         },
         onPenTapped: () {
-          setState(() {
-            pickerColor = Colors.red;
-          });
+          _controller.forward();
         },
       ),
-      body: Stack(
-        children: <Widget>[
-          GestureDetector(
-            onPanStart: (startDetails) {
-              // oldList = localList;
-              final renderBox = context.findRenderObject() as RenderBox;
-              final localPosition =
-                  renderBox.globalToLocal(startDetails.globalPosition);
-              localList.add(localPosition);
-              offSetController.offsetController.sink.add(localList);
-              // print(startDetails.globalPosition.dx);
-            },
-            onPanUpdate: (updateDetails) {
-              final renderBox = context.findRenderObject() as RenderBox;
-              final localPosition =
-                  renderBox.globalToLocal(updateDetails.globalPosition);
-              localList.add(localPosition);
-              offSetController.offsetController.sink.add(localList);
-            },
-            onPanEnd: (downDetails) {
-              localList.add(null);
-              offSetController.offsetController.sink.add(localList);
-            },
-            child: StreamBuilder<List<Offset>>(
-                stream: offSetController.offsetController.stream,
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<Offset>> snapshot) {
-                  return snapshot.data == null
-                      ? Container(
-                          color: Colors.transparent,
-                          alignment: Alignment.center,
-                          child: Container(child: Text('Tap to Start')),
-                        )
-                      : CustomPaint(
-                          painter: WhiteBoardPainter(
-                              offsetList: snapshot.data,
-                              color: pickerColor,
-                              blendMode: blendMode),
-                          child: Container(),
-                        );
-                }),
-          ),
-          Container(
-            child: CustomPaint(
-              painter: DotPainter(),
+      body: GestureDetector(
+        onTap: () {
+          if (_controller.status == AnimationStatus.completed) {
+            _controller.reset();
+            return;
+          }
+        },
+        child: Stack(
+          children: <Widget>[
+            GestureDetector(
+              onPanStart: (startDetails) {
+                // oldList = localList;
+                final renderBox = context.findRenderObject() as RenderBox;
+                final localPosition =
+                    renderBox.globalToLocal(startDetails.globalPosition);
+                localList.add(localPosition);
+                offSetController.offsetController.sink.add(localList);
+                // print(startDetails.globalPosition.dx);
+              },
+              onPanUpdate: (updateDetails) {
+                final renderBox = context.findRenderObject() as RenderBox;
+                final localPosition =
+                    renderBox.globalToLocal(updateDetails.globalPosition);
+                localList.add(localPosition);
+                offSetController.offsetController.sink.add(localList);
+              },
+              onPanEnd: (downDetails) {
+                localList.add(null);
+                offSetController.offsetController.sink.add(localList);
+              },
+              child: StreamBuilder<List<Offset>>(
+                  stream: offSetController.offsetController.stream,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<Offset>> snapshot) {
+                    return snapshot.data == null
+                        ? Container(
+                            color: Colors.transparent,
+                            alignment: Alignment.center,
+                            child: Container(child: Text('Tap to Start')),
+                          )
+                        : CustomPaint(
+                            painter: WhiteBoardPainter(
+                                offsetList: snapshot.data,
+                                color: pickerColor,
+                                strokeWidth: _sliderValue,
+                                blendMode: blendMode),
+                            child: Container(),
+                          );
+                  }),
             ),
-          )
-        ],
+            Container(
+              child: CustomPaint(
+                painter: DotPainter(),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Transform.translate(
+                offset: Offset(
+                  0,
+                  animation.value,
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.15),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('Pointer Size:'),
+                      Text(_sliderValue.toStringAsPrecision(2)),
+                      Slider(
+                          value: _sliderValue,
+                          min: 10,
+                          max: 40,
+                          label: 'Size',
+                          onChanged: (value) {
+                            setState(() {
+                              _sliderValue = value;
+                            });
+                          }),
+                      Container(
+                        width: 40,
+                        height: 40,
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: _sliderValue,
+                          width: _sliderValue,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius:
+                                  BorderRadius.circular(_sliderValue * 20)),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
   }
 }
 
 class WhiteBoardPainter extends CustomPainter {
-  WhiteBoardPainter({this.offsetList, this.blendMode, this.color});
+  WhiteBoardPainter(
+      {this.offsetList, this.blendMode, this.color, this.strokeWidth});
   Animation<Color> animation;
   List<Offset> offsetList;
   BlendMode blendMode;
+  double strokeWidth;
   Color color;
   @override
   @override
   void paint(Canvas canvas, Size size) {
     // TODO: implement paint
-    Paint paint = Paint()
+
+    final double _dotsDx = 40;
+    final double _dotsDy = 40;
+    Paint paint = Paint();
+
+    /// paint for the background dots
+    paint
+      ..style = PaintingStyle.stroke
+      ..color = Colors.grey.withOpacity(0.4)
+      ..strokeWidth = 1.0;
+
+    /// draw Background dots for accuracy
+    ///
+    for (double dx = 0; dx <= size.width; dx = dx + _dotsDx) {
+      for (double dy = 0; dy <= size.height; dy = dy + _dotsDy) {
+        canvas.drawCircle(Offset(dx, dy), 2, paint);
+      }
+    }
+
+    /// define paint for the brush
+    ///
+    paint
       ..color = color //Color.fromRGBO(97, 190, 162, 1.0)
-      ..strokeWidth = 100.0
-      ..strokeJoin = StrokeJoin.round
-      ..blendMode = blendMode
-      ..imageFilter = ImageFilter.blur()
+      ..strokeWidth = strokeWidth
+      // ..strokeJoin = StrokeJoin.round
+      // ..blendMode = blendMode
+      // ..imageFilter = ImageFilter.blur()
       ..style = PaintingStyle.fill;
     // final c = size.center(Offset.zero);
 
@@ -144,11 +243,6 @@ class WhiteBoardPainter extends CustomPainter {
             Offset(offsetList[i].dx, offsetList[i].dy - kToolbarHeight),
             Offset(offsetList[i + 1].dx, offsetList[i + 1].dy - kToolbarHeight),
             paint);
-        // } else if (offsetList[i] != null && offsetList[i + 1] == null) {
-        //   canvas.drawPoints(
-        //       PointMode.points,
-        //       [Offset(offsetList[i].dx, offsetList[i].dy - kToolbarHeight)],
-        //       paint);
       }
     }
   }
